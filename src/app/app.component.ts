d6ac9f00f389c1bb667ec4def7b1ad48a4c12979
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2018-01-19 00:20:02 +0200

    add loader and update style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,20 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
-import * as Scrapper from 'jobs-dou-statistics-scrapper';
+import * as scrapper from 'jobs-dou-statistics-scrapper';
+
+
+function loading(target, key, descriptor) {
+  if(descriptor === undefined) {
+    descriptor = Object.getOwnPropertyDescriptor(target, key);
+  }
+  const originalMethod = descriptor.value;
+
+  descriptor.value = async function(...args){
+    this.loading = true;
+    const result = await originalMethod.apply(this, args);
+    this.loading = false; 
+    return result;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -11,12 +26,26 @@ export class AppComponent {
 
   categories = [];
   companies = [];
+  vacanciesByCompany = {}
 
   constructor(private cd: ChangeDetectorRef) {}
 
+  @loading
   async initGrab() {
-    this.categories = await Scrapper.grabCategories();
-    this.companies = await Scrapper.grabTop10Companies();
-    console.log(this);
+    this.categories = await scrapper.grabCategories();
+    this.companies = await scrapper.grabTop10Companies();
+    console.log('initGrab end!');
   }
+
+  @loading
+  async grab() {
+    const populateCategories = await scrapper.populateCategories(this.categories);
+
+    for(const company of this.companies){
+        this.vacanciesByCompany[company.id] = await scrapper.grabVacancies(company.id, populateCategories);
+    }
+    console.log('initGrab end!');
+  }
+
+
 }
