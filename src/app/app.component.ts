afdf6c3cfff6cc29051f61b2b535c03b621f430b
Vitaliy-Yarovuy <vitaliy.yxz@gmail.com>
2018-01-21 16:23:26 +0200

    update code according changes in scrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import { Component} from '@angular/core';
 import * as scrapper from 'jobs-dou-statistics-scrapper';
 
 
 function loading(target, key, descriptor) {
-  if(descriptor === undefined) {
+  if (descriptor === undefined) {
     descriptor = Object.getOwnPropertyDescriptor(target, key);
   }
   const originalMethod = descriptor.value;
 
-  descriptor.value = async function(...args){
+  descriptor.value = async function (...args) {
     this.loading = true;
     const result = await originalMethod.apply(this, args);
-    this.loading = false; 
+    this.loading = false;
     return result;
-  }
+  };
 }
 
 @Component({
@@ -26,26 +26,39 @@ export class AppComponent {
 
   categories = [];
   companies = [];
-  vacanciesByCompany = {}
+  vacanciesGrabbed = false;
 
-  constructor(private cd: ChangeDetectorRef) {}
+  readonly vacanciesFields = ['id',
+    'company',
+    'title',
+    'href',
+    'desc',
+    'date',
+    'cities',
+    'salary'];
+
+  constructor() {}
+
+
+  cutDown(text, count = 50) {
+    return text && text.length > count ? text.substr(0, count) + '...' : text;
+  }
 
   @loading
   async initGrab() {
     this.categories = await scrapper.grabCategories();
     this.companies = await scrapper.grabTop10Companies();
-    console.log('initGrab end!');
+    console.log('initGrab end!', this);
   }
 
   @loading
   async grab() {
-    const populateCategories = await scrapper.populateCategories(this.categories);
 
-    for(const company of this.companies){
-        this.vacanciesByCompany[company.id] = await scrapper.grabVacancies(company.id, populateCategories);
+    for (const category of this.categories) {
+      category.vacancies = await scrapper.grabVacancies(category.id);
     }
-    console.log('initGrab end!');
+    this.vacanciesGrabbed = true;
+    console.log('grab end!');
   }
-
 
 }
